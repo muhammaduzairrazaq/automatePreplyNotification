c9850f4fb4dee9692722cccd6a4c8ed6404de75c
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-09-08 13:05:51 +0500

    Added error handling in sendemail function

--- path/to/venv/bin/activate.fish
+++ path/to/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/iamuzair/Documents/Projects/automatePreplyNotification/path/to/venv"
+set -gx VIRTUAL_ENV "/home/black/Documents/Projects/automatePreplyNotification/path/to/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
